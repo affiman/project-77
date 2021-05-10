48159a4779810a42f0eefd3867aec85ea52b1b5c
affiman <78100788+affiman@users.noreply.github.com>
2021-05-10 22:07:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+console.log ("My first console project");
+
+var first_name="Abhih";
+
+var last_name="Pasricha";
+
+var full_name=first_name.concat(last_name);
+
+console.log (full_name);
+
+var number1 = 10;
+var number2 = 5;
+var addition = number1+number2;
+
+console.log (addition);
+
+
+var subtract = number1-number2;
+
+console.log (subtract);
+
+var multiply = number1*number2;
+
+console.log (multiply);
+
+var divide = number1/number2;
+
+console.log (divide);
+
